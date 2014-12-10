ae0e908200341450f30d2cff95e009b46655fae8
philipjrosen <philipjrosen@gmail.com>
2014-12-10 15:45:08 -0800

    stack func-shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {
+    storage: {},
+    counter: 0,
+  };
+  _.extend(instance, stackMethods);
+
+  return instance;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value){
+    this.storage[this.counter++] = value;
+  },
+  pop: function(){
+    if (this.counter > 0){
+      var removed = this.storage[--this.counter];
+      delete this.storage[this.counter];
+      return removed;
+    }
+  },
+  size: function(){
+      return this.counter;
+  }
+};
