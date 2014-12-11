e664a36a9d09035b515ff0ec62092f2fd6ad3761
philipjrosen <philipjrosen@gmail.com>
2014-12-11 00:23:45 -0800

    Incomplete refactor for hashing conflicts

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,17 +3,34 @@ var HashTable = function(){
   this._storage = LimitedArray(this._limit);
 };
 
+//REFACTOR:
+//Handling hash conflicts: store conflicting key-value
+//pairs in sub-arrays within this._storage.storage
+
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if (Array.isArray(this._storage.get(i))){
+    this._storage.get(i).push(v);
+  } else {
+    var subStorage = [];
+    subStorage.push(v);
+    this._storage.set(i,subStorage);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var result = this._storage.get(i);
+  return result === undefined ? null : result;
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(element, index, storage){
+    if(i===index){
+      delete storage[index];
+    }
+  });
 };
 
 
