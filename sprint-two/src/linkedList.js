a79fc97c9845a811e7631c357aaed7f32858be63
philipjrosen <philipjrosen@gmail.com>
2014-12-10 18:22:55 -0800

    Add LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,33 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node(value);
+    if (list.head === null){
+      list.tail = node;
+      list.head = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function(){
+    var temp = list.head.value;
+    list.head = list.head.next;
+    return temp;
   };
 
   list.contains = function(target){
+    var result = false;
+    var currentNode =list.head;
+    while(currentNode !== null){
+      if (currentNode.value === target){
+        result = true;
+      }
+      currentNode = currentNode.next;
+    }
+    return result;
   };
-
   return list;
 };
 
