9db53288a4dbed0929e5c110c78f0df045e8a440
philipjrosen <philipjrosen@gmail.com>
2014-12-10 22:55:14 -0800

    add set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,25 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item){
+  return typeof _.find(this._storage, function(value) {
+    return value === item;
+  }) === 'string';
 };
 
 setPrototype.remove = function(item){
+  var removed = this._storage[item];
+  delete this._storage[item];
+  return removed;
 };
 
 /*
