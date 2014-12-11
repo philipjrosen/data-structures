a76d95de9226938736659b97b5909230cf7e7fc0
philipjrosen <philipjrosen@gmail.com>
2014-12-10 20:55:35 -0800

    add tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -15,14 +17,27 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target){
-
+  var found = false;
+  if (this.children.length === 0) {
+    if (this.value === target) {
+      found = true;
+    }
+  } else {
+    found = _.some(this.children, function(child) {
+      return child.value === target || child.contains(target);
+    });
+  }
+  return found;
 };
 
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above func
+ addChild ~ O(1) ie constant time
+ contains ~ O(n!) eg a tree with unbounded children, with unbounded depth, and for which each level has unbounded children
  */
