ea4a2a1714163059d9f0f9ca475364f3935dffb1
philipjrosen <philipjrosen@gmail.com>
2014-12-10 22:31:12 -0800

    add graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,71 @@
 
 
 var Graph = function(){
+  this.nodes = [];
+
+
 };
 
-Graph.prototype.addNode = function(node){
+Graph.prototype.addNode = function(value){
+  var node = { edges: [], value: value};
+  this.nodes.push(node);
 };
 
-Graph.prototype.contains = function(node){
+Graph.prototype.contains = function(target){
+  return _.some(this.nodes, function(node){
+    return node.value === target;
+  });
 };
 
-Graph.prototype.removeNode = function(node){
+Graph.prototype.removeNode = function(target){
+  var del = this.nodes.indexOf(_.find(this.nodes, function(node){
+      return node.value === target;
+  }));
+  if (del !== -1){
+    this.nodes.splice(del, 1);
+  }
 };
 
-Graph.prototype.hasEdge = function(fromNode, toNode){
+Graph.prototype.hasEdge = function(fromNodeValue, toNodeValue){
+  var origin = _.find(this.nodes, function(node){
+      return node.value === fromNodeValue;
+  });
+  var destination = _.find(this.nodes, function(node){
+    return node.value === toNodeValue;
+  });
+  return (origin.edges.indexOf(destination) >= 0) ? true : false;
 };
 
-Graph.prototype.addEdge = function(fromNode, toNode){
+Graph.prototype.addEdge = function(fromNodeValue, toNodeValue){
+  var origin = _.find(this.nodes, function(node){
+      return node.value === fromNodeValue;
+    });
+  var destination = _.find(this.nodes, function(node){
+      return node.value === toNodeValue;
+    });
+  origin.edges.push(destination);
+  destination.edges.push(origin);
+
 };
 
-Graph.prototype.removeEdge = function(fromNode, toNode){
+Graph.prototype.removeEdge = function(fromNodeValue, toNodeValue){
+  var origin = _.find(this.nodes, function(node){
+      return node.value === fromNodeValue;
+  });
+  var destination = _.find(this.nodes, function(node){
+    return node.value === toNodeValue;
+  });
+  var dIndex = origin.edges.indexOf(destination);
+  var oIndex = destination.edges.indexOf(origin);
+  origin.edges.splice(dIndex,1);
+  destination.edges.splice(oIndex,1);
 };
 
 Graph.prototype.forEachNode = function(cb){
+  _.each(this.nodes, function(node){
+    cb(node.value);
+  });
+
 };
 
 /*
