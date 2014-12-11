8703b262bab0555632dcfc5c45cbd6572a2b6dd8
philipjrosen <philipjrosen@gmail.com>
2014-12-11 10:38:31 -0800

    add insert for BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,39 @@
-var BinarySearchTree = function(value){
+var BinarySearchTree = function(value) {
+  var tree = Object.create(bstMethods);
+  tree.left = null;
+  tree.right = null;
+  tree.value = value;
+  return tree;
 };
 
+var bstMethods = {
+  insert: function(value) {
+    var child = BinarySearchTree(value);
+    if (this.value === value) {
+      return;
+    } else if (value < this.value ) {
+      if (this.left === null) {
+        this.left = child;
+      } else {
+        this.left.insert(child.value);
+      }
+    } else if (value > this.value ) {
+      if (this.right === null) {
+        this.right = child;
+      } else {
+        this.right.insert(child.value);
+      }
+    }
+  },
+
+  contains: function() {
+
+  },
+
+  depthFirstLog: function() {
+
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
