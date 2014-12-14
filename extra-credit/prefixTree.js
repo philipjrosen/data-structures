3a1ea88bd29cc89166a9b6b015c82552d4a747ec
philipjrosen <philipjrosen@gmail.com>
2014-12-13 18:26:42 -0800

    started PrefixTree and tests

---
+++ extra-credit/prefixTree.js
@@ -0,0 +1,18 @@
+var PrefixTree = function(){
+  var tree = Object.create(prefixTreeMethods);
+  tree.children = [];
+
+  return tree;
+};
+
+var prefixTreeMethods = {
+  hasChildren: function(){},
+
+  findString: function(){},
+
+  newNode: function() {},
+
+  returnWord: function() {},
+
+
+};
---
+++ extra-credit/spec/prefixTree.js
@@ -0,0 +1,28 @@
+//working code
+/* global binarySearchTree, describe, it, expect, should */
+describe('PrefixTree()', function () {
+  var prefixTree;
+  beforeEach(function() {
+    prefixTree = PrefixTree();
+  });
+
+  it('should have a hasChildren, findString, returnWord function', function() {
+    expect(prefixTree.hasChildren).to.be.a("function");
+    expect(prefixTree.newNode).to.be.a("function");
+    expect(prefixTree.findString).to.be.a("function");
+    expect(prefixTree.returnWord).to.be.a("function");
+  });
+
+  it('should have a property: children array', function() {
+    expect(prefixTree.children).to.be.instanceof(Array);
+  });
+
+  // it('should have a parentNode', function() {
+  //   expect(prefixTree.hasChildren).to.be.a("function");
+  // });
+
+  // it('should have a parentNode', function() {
+  //   expect(prefixTree.hasChildren).to.be.a("function");
+  // });
+
+});
